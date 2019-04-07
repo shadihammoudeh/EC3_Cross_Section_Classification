9ff1e88897313428589e2c2a8b527c353d8ea0c1
Bespoke Engineering <shadi.hammoudeh@icloud.com>
2019-04-07 22:32:35 +0100

    Create UIBUttons for main.storeyboard programmatically and calculate their size.

--- EC3_Cross_Section_Classification/Controller/MainScreen.swift
+++ EC3_Cross_Section_Classification/Controller/MainScreen.swift
@@ -10,100 +10,137 @@ import UIKit
 
 class MainScreen: UIViewController {
     
-    @IBOutlet weak var newFileButton: UIButton!
+    let buttonTitle: [String: String] = ["newFileButtonTitle": "New File...", "openFileButtonTitle": "Open", "documentationButtonTitle": "Documentation", "emailUsButtonTitle": "Email Us"]
     
-    @IBOutlet weak var openFileButton: UIButton!
+    let buttonIconImage: [String: String] = ["newFileButtonIcon": "newFileButtonIcon", "openFileButtonIcon": "openFileButtonIcon", "documentationButtonIcon": "documentationIcon", "emailUsButtonIcon": "emailUsIcon"]
     
-    @IBOutlet weak var documentationButton: UIButton!
+    let differentIphoneScreens: [String: Double] = ["iPhoneXsMax,XR": 896, "iPhoneX,Xs": 812, "iPhone6+,6s+,7+,8+": 736, "iPhone6,6s,7,8": 667, "iPhone5,5s,5c,SE": 568]
     
-    @IBOutlet weak var emailUsButton: UIButton!
+    let differentBackgroundImageSize: [String: Double] = ["iPhoneXsMax,XR": 774, "iPhoneX,Xs": 690, "iPhone6+,6s+,7+,8+": 672, "iPhone6,6s,7,8": 603, "iPhone5,5s,5c,SE": 504]
     
-    @IBOutlet weak var backgroundImage: UIImageView!
+    // vertical position of first button item for iphoneXsmax = 64(navBarHeight) + 100 + 32(buttonheight /2)
     
-    let differentIphoneModels: [String] = ["iPhoneXsMax&XR", "iPhoneX&Xs", "iPhone6Plus,6sPlus,7Plus&8Plus", "iPhone6,6s,7&8", "iPhone5,5s,5c&SE"]
+    // vertical position of second button for iphoneXsMax = 64 (navbar) + 100 + 64(height of first bar) + [total background image height - 100 - 100 - (64*4)]/3 - 64/2
     
-    let backgroundImageHeightForDifferentiPhonesScreens: [CGFloat] = [774, 690, 672, 603, 504]
-    
-    override func viewWillAppear(_ animated: Bool) {
-        
-        // The below Array has been created for all buttons in this ViewController in order to affect the layout of all of them when the modifyButtonsLayout Method is called later on:
-        
-        let  buttonsArray = [newFileButton, openFileButton, documentationButton, emailUsButton]
-        
-        // The below code loops through every UIButton included in the buttonsArray and then pass the UIBUtton into the modifyButtonsLayout Method in order to alter its layout as desired:
-        
-        for button in buttonsArray {
-            
-            modifyButtonsLayout(buttonName: button!)
-            
-        }
-        
-        let currentBackgroundImageHeight = backgroundImage.frame.height
-        
-        modifyButtonsConstraints(backgroundImageHeight: currentBackgroundImageHeight)
-        
-    }
+    // viewDidLoad() Method gets called after loading the view. Thus, any UI related changes should be done inside it:
     
     override func viewDidLoad() {
         
         super.viewDidLoad()
         
-        // Do any additional setup after loading the view.
-    
-    }
+        let currentIphoneScreenHeight = Double(UIScreen.main.bounds.size.height)
+        
+        let navigationBarHeight = self.navigationController?.navigationBar.frame.size.height
+        
+        let window = UIApplication.shared.keyWindow
+        
+        let topPadding = window?.safeAreaInsets.top
+        
+        let bottomPadding = window?.safeAreaInsets.bottom
+        
+        if currentIphoneScreenHeight == differentIphoneScreens["iPhoneXsMax,XR"] {
+            
+            let newFileButtonPositionAndSize: [String: Double] = ["xCoordinate": 100, "yCoordinate": Double(topPadding!) + Double(navigationBarHeight!) + 100, "width": 185, "height": 64]
+        
+            createAnewButtonProgrammatically(buttonXpostion: 100, buttonYposition: newFileButtonPositionAndSize["yCoordinate"]!, buttonWidth: newFileButtonPositionAndSize["width"]!, buttonHeight: newFileButtonPositionAndSize["height"]!, buttonCornerRadius: 20, buttonBackgroundColour: .red, buttonTextSize: 18, buttonTextColourInNormalCondition: .white, buttonTextColourInHighlightedCondition: .blue, buttonTextTitle: buttonTitle["newFileButtonTitle"]!, buttonIconImage: buttonIconImage["newFileButtonIcon"]!).addTarget(self, action: #selector(openFileButtonPressed(sender:)), for: .touchUpInside)
+            
+            let openFileButtonPositionAndSize: [String: Double] = ["xCoordinate": 100, "yCoordinate": 300, "width": 115, "height": 64]
+            
+            createAnewButtonProgrammatically(buttonXpostion: 100, buttonYposition: openFileButtonPositionAndSize["yCoordinate"]!, buttonWidth: 150, buttonHeight: 64, buttonCornerRadius: 20, buttonBackgroundColour: .red, buttonTextSize: 18, buttonTextColourInNormalCondition: .white, buttonTextColourInHighlightedCondition: .blue, buttonTextTitle: buttonTitle["openFileButtonTitle"]!, buttonIconImage: buttonIconImage["openFileButtonIcon"]!).addTarget(self, action: #selector(openFileButtonPressed(sender:)), for: .touchUpInside)
+            
+            let documentationButtonPositionAndSize: [String: Double] = ["xCoordinate": 100, "yCoordinate": 100, "width": 200, "height": 64]
 
-    // The below Method modifies the layout of all buttons on this ViewController:
-    
-    func modifyButtonsLayout(buttonName: UIButton) {
-        
-        buttonName.layer.cornerRadius = 20 // This line of code is needed in order to introduce corners to the button boarders rather than having them at 90 degrees angles.
-        
-        buttonName.showsTouchWhenHighlighted = true // This line of code make the button icon glow whenever the user press on the button.
-        
-        buttonName.setTitleColor(UIColor .white, for: UIControl.State.highlighted) // This line of code changes the text inside the button to white whenever the user click on it.
-        
-        buttonName.translatesAutoresizingMaskIntoConstraints = false // This line of code is needed in order to enable all the constraints that we are going to apply later on to the UIButton elements.
-        
-        
-    }
-    
-    // The below Method sets the constraints required for each button depending on the iPhone screen size the user is using:
-    
-    func modifyButtonsConstraints(backgroundImageHeight: CGFloat) {
-        
-        if Int(backgroundImageHeight) == 774 { // 774 points corresponds to iPhone Xs Max screen size:
+            createAnewButtonProgrammatically(buttonXpostion: 100, buttonYposition: 350, buttonWidth: 150, buttonHeight: 64, buttonCornerRadius: 20, buttonBackgroundColour: .red, buttonTextSize: 18, buttonTextColourInNormalCondition: .white, buttonTextColourInHighlightedCondition: .blue, buttonTextTitle: buttonTitle["documentationButtonTitle"]!, buttonIconImage: buttonIconImage["documentationButtonIcon"]!).addTarget(self, action: #selector(openFileButtonPressed(sender:)), for: .touchUpInside)
             
-            let newFileButtonTopConstraintToTopOfBackgroundImage: CGFloat = 100
+            let emailUsButtonPositionAndSize: [String: Double] = ["xCoordinate": 100, "yCoordinate": 100, "width": 150, "height": 64]
+
+            createAnewButtonProgrammatically(buttonXpostion: 100, buttonYposition: 450, buttonWidth: 150, buttonHeight: 64, buttonCornerRadius: 20, buttonBackgroundColour: .red, buttonTextSize: 18, buttonTextColourInNormalCondition: .white, buttonTextColourInHighlightedCondition: .blue, buttonTextTitle: buttonTitle["emailUsButtonTitle"]!, buttonIconImage: buttonIconImage["emailUsButtonIcon"]!).addTarget(self, action: #selector(openFileButtonPressed(sender:)), for: .touchUpInside)
             
-            let emailUsButtonBottomConstraintToBottomOfBackgroundImage: CGFloat = -100
+        } else if currentIphoneScreenHeight == differentIphoneScreens["iPhoneX,Xs"] {
             
-            let openButtonConstraintAndDocumentationButtonConstraint: Int = (Int(backgroundImageHeight) - Int(newFileButtonTopConstraintToTopOfBackgroundImage) + Int(emailUsButtonBottomConstraintToBottomOfBackgroundImage) - 256)/3
+            let newFileButtonPositionAndSize: [String: Double] = ["xCoordinate": 100, "yCoordinate": Double(topPadding!) + Double(navigationBarHeight!) + 100-(differentBackgroundImageSize["iPhoneXsMax,XR"]!-differentBackgroundImageSize["iPhoneX,Xs"]!)/5, "width": 185, "height": 64]
             
-            newFileButton.topAnchor.constraint(equalTo: backgroundImage.topAnchor, constant: newFileButtonTopConstraintToTopOfBackgroundImage).isActive = true
+            createAnewButtonProgrammatically(buttonXpostion: 100, buttonYposition: newFileButtonPositionAndSize["yCoordinate"]!, buttonWidth: newFileButtonPositionAndSize["width"]!, buttonHeight: newFileButtonPositionAndSize["height"]!, buttonCornerRadius: 20, buttonBackgroundColour: .red, buttonTextSize: 18, buttonTextColourInNormalCondition: .white, buttonTextColourInHighlightedCondition: .blue, buttonTextTitle: buttonTitle["newFileButtonTitle"]!, buttonIconImage: buttonIconImage["newFileButtonIcon"]!).addTarget(self, action: #selector(openFileButtonPressed(sender:)), for: .touchUpInside)
             
-            emailUsButton.bottomAnchor.constraint(equalTo: backgroundImage.bottomAnchor, constant: emailUsButtonBottomConstraintToBottomOfBackgroundImage).isActive = true
+            let openFileButtonPositionAndSize: [String: Double] = ["xCoordinate": 100, "yCoordinate": 300, "width": 115, "height": 64]
             
-            openFileButton.topAnchor.constraint(equalTo: newFileButton.bottomAnchor, constant: CGFloat(openButtonConstraintAndDocumentationButtonConstraint)).isActive = true
+            createAnewButtonProgrammatically(buttonXpostion: 100, buttonYposition: openFileButtonPositionAndSize["yCoordinate"]!, buttonWidth: 150, buttonHeight: 64, buttonCornerRadius: 20, buttonBackgroundColour: .red, buttonTextSize: 18, buttonTextColourInNormalCondition: .white, buttonTextColourInHighlightedCondition: .blue, buttonTextTitle: buttonTitle["openFileButtonTitle"]!, buttonIconImage: buttonIconImage["openFileButtonIcon"]!).addTarget(self, action: #selector(openFileButtonPressed(sender:)), for: .touchUpInside)
             
-            documentationButton.topAnchor.constraint(equalTo: openFileButton.bottomAnchor, constant: CGFloat(openButtonConstraintAndDocumentationButtonConstraint)).isActive = true
+            let documentationButtonPositionAndSize: [String: Double] = ["xCoordinate": 100, "yCoordinate": 400, "width": 200, "height": 64]
             
-        } else { // For all other iPhones:
+            createAnewButtonProgrammatically(buttonXpostion: 100, buttonYposition: 350, buttonWidth: 150, buttonHeight: 64, buttonCornerRadius: 20, buttonBackgroundColour: .red, buttonTextSize: 18, buttonTextColourInNormalCondition: .white, buttonTextColourInHighlightedCondition: .blue, buttonTextTitle: buttonTitle["documentationButtonTitle"]!, buttonIconImage: buttonIconImage["documentationButtonIcon"]!).addTarget(self, action: #selector(openFileButtonPressed(sender:)), for: .touchUpInside)
             
-            let newFileButtonTopConstraintToTopOfBackgroundImage: Int = 100 - ((774 - Int(backgroundImageHeight))/5)
+            let emailUsButtonPositionAndSize: [String: Double] = ["xCoordinate": 100, "yCoordinate": 500, "width": 150, "height": 64]
             
-            let emailUsButtonBottomConstraintToBottomOfBackgroundImage: Int = -100 + ((774 - Int(backgroundImageHeight))/5)
-            
-            let openButtonConstraintAndDocumentationButtonConstraint: Int = (Int(backgroundImageHeight) - Int(newFileButtonTopConstraintToTopOfBackgroundImage) + Int(emailUsButtonBottomConstraintToBottomOfBackgroundImage) - 256)/3
-            
-            newFileButton.topAnchor.constraint(equalTo: backgroundImage.topAnchor, constant: CGFloat(newFileButtonTopConstraintToTopOfBackgroundImage)).isActive = true
-            
-            emailUsButton.bottomAnchor.constraint(equalTo: backgroundImage.bottomAnchor, constant: CGFloat(emailUsButtonBottomConstraintToBottomOfBackgroundImage)).isActive = true
-            
-            openFileButton.topAnchor.constraint(equalTo: newFileButton.bottomAnchor, constant: CGFloat(openButtonConstraintAndDocumentationButtonConstraint)).isActive = true
-            
-            documentationButton.topAnchor.constraint(equalTo: openFileButton.bottomAnchor, constant: CGFloat(openButtonConstraintAndDocumentationButtonConstraint)).isActive = true
+            createAnewButtonProgrammatically(buttonXpostion: 100, buttonYposition: 450, buttonWidth: 150, buttonHeight: 64, buttonCornerRadius: 20, buttonBackgroundColour: .red, buttonTextSize: 18, buttonTextColourInNormalCondition: .white, buttonTextColourInHighlightedCondition: .blue, buttonTextTitle: buttonTitle["emailUsButtonTitle"]!, buttonIconImage: buttonIconImage["emailUsButtonIcon"]!).addTarget(self, action: #selector(openFileButtonPressed(sender:)), for: .touchUpInside)
             
         }
+        
+    }
+    
+    func createAnewButtonProgrammatically (buttonXpostion xCoordinate: Double, buttonYposition yCoordinate: Double, buttonWidth width: Double, buttonHeight height: Double, buttonCornerRadius radius: CGFloat, buttonBackgroundColour colour: UIColor, buttonTextSize fontSize: CGFloat, buttonTextColourInNormalCondition normalTextColour: UIColor, buttonTextColourInHighlightedCondition highlightedTextColour: UIColor, buttonTextTitle title: String, buttonIconImage iconImage: String) -> UIButton {
+        
+        // In the below we are creating a UIButton object and assign it to the bottom variable:
+        
+        let newButton = UIButton(type: .system)
+        
+        // In the below we are adding a frame to this Variable by creating a CGRect using the values we have dfined earlier on:
+        
+        newButton.frame = CGRect(x: xCoordinate, y: yCoordinate, width: width, height: height)
+        
+        newButton.backgroundColor = colour
+        
+        newButton.layer.cornerRadius = radius
+        
+        newButton.titleLabel?.font = UIFont(name: "Helvetica Neue", size: fontSize)
+        
+        newButton.setTitleColor(normalTextColour, for: .normal)
+        
+        newButton.setTitleColor(highlightedTextColour, for: .highlighted)
+        
+        newButton.setTitle(title, for: .normal)
+        
+        newButton.contentHorizontalAlignment = UIControl.ContentHorizontalAlignment.center
+        
+        newButton.center.x = self.view.center.x
+        
+        if let newFileButtonImage = UIImage(named: iconImage) {
+            
+            newButton.setImage(newFileButtonImage, for: .normal)
+            
+        }
+        
+        self.view.addSubview(newButton)
+
+        return newButton
+        
+    }
+    
+    @objc func newFileButtonPressed(sender : UIButton) {
+        
+        sender.tag = 1
+        
+//        let alert = UIAlertController(title: "Clicked", message: "You have clicked on the button", preferredStyle: .alert)
+//
+//        self.present(alert, animated: true, completion: nil)
+        
+    }
+    
+    @objc func openFileButtonPressed(sender : UIButton) {
+    
+        sender.tag = 2
+
+    }
+    
+    @objc func documentationButtonPressed(sender : UIButton) {
+        
+        sender.tag = 3
+        
+    }
+    
+    @objc func emailUsButtonPressed(sender : UIButton) {
+        
+        sender.tag = 4
         
     }
 
